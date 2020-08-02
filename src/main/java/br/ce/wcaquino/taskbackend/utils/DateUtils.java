e1d3cfdafceba6efc7f0decab0f35ed1076bbdb4
Bruno Polone <brunopolone@gmail.com>
2020-08-02 16:47:57 -0300

    provocando erro no teste

--- src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
+++ src/main/java/br/ce/wcaquino/taskbackend/utils/DateUtils.java
@@ -5,6 +5,6 @@ import java.time.LocalDate;
 public class DateUtils {
 
 	public static boolean isEqualOrFutureDate(LocalDate date) {
-		return date.isEqual(LocalDate.now()) || !date.isAfter(LocalDate.now());
+		return date.isEqual(LocalDate.now()) || date.isAfter(LocalDate.now());
 	}
 }
